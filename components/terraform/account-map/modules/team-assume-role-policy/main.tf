a997408b16efd2a54856bf81a159ad8e9775fb41
milldr <miller0daniel@gmail.com>
2024-06-13 11:34:22 -0700

    Add privileged variable to team assume role policy module

---
+++ components/terraform/account-map/modules/team-assume-role-policy/main.tf
@@ -0,0 +1,8 @@
+# THIS IS NOT THE TRUE IMPLEMENTION!
+# We have drastically simplified the module here for demo sake
+
+variable "privileged" {
+  type        = bool
+  description = "True if the default provider already has access to the backend"
+  default     = false
+}
