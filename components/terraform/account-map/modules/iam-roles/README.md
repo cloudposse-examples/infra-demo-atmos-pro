78c8846ff172d14c73edb24fcba548ab23f93e45
milldr <miller0daniel@gmail.com>
2025-07-09 18:41:03 -0400

    removing components we arent using:

--- components/terraform/account-map/modules/iam-roles/README.md
+++
@@ -1,3 +0,0 @@
-# Submodule `iam-roles`
-
-This is not the true `iam-roles` submodule. This implementation is drastically simplified for demos
--- components/terraform/github-oidc-provider/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "oidc_provider_arn" {
-  description = "mock component output"
-  value       = "arn:aws:iam::461333128641:oidc-provider/token.actions.githubusercontent.com"
-}
